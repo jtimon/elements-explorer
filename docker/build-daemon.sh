e989886bf2c1cf3b27f3e6939e7d64ae24c33b18
Jorge Timón <jtimon@jtimon.cc>
2017-07-19 06:24:16 +0200

    Docker: Daemon: s/56/$NUM_JOBS/

--- docker/build-daemon.sh
+++ docker/build-daemon.sh
@@ -1,4 +1,10 @@
 ./autogen.sh
 # TODO --disable-wallet ?
 ./configure --without-gui --with-incompatible-bdb
-make -j56
+
+NUM_JOBS=4
+if [ -f /proc/cpuinfo ]; then
+    NUM_JOBS=$(cat /proc/cpuinfo | grep ^processor | wc -l)
+fi
+ 
+make -j$NUM_JOBS
