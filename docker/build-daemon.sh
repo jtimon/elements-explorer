ee3c9da26f9f9a0b51369f3ac867bbccc6e75540
Jorge Timón <jtimon@jtimon.cc>
2017-07-26 20:40:40 +0200

    Docker: Build only the daemon

--- docker/build-daemon.sh
+++ docker/build-daemon.sh
@@ -1,9 +1,10 @@
 ./autogen.sh
 ./configure --disable-wallet --without-gui --with-incompatible-bdb
 
+DAEMON_TARGET=${1:-src/elementsd}
 NUM_JOBS=4
 if [ -f /proc/cpuinfo ]; then
     NUM_JOBS=$(cat /proc/cpuinfo | grep ^processor | wc -l)
 fi
- 
-make -j$NUM_JOBS
+
+make $DAEMON_TARGET -j$NUM_JOBS
