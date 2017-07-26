9d1cf1b57d28a999f06b1648b4f50a1405b254cf
Jorge Timón <jtimon@jtimon.cc>
2017-07-26 09:13:10 +0200

    Docker: ./configure --disable-wallet

--- docker/build-daemon.sh
+++ docker/build-daemon.sh
@@ -1,6 +1,5 @@
 ./autogen.sh
-# TODO --disable-wallet ?
-./configure --without-gui --with-incompatible-bdb
+./configure --disable-wallet --without-gui --with-incompatible-bdb
 
 NUM_JOBS=4
 if [ -f /proc/cpuinfo ]; then
