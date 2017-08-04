4c9dab45262688eeeb04270adffbaedb2e898778
Jorge Timón <jtimon@jtimon.cc>
2017-08-04 03:46:41 +0200

    Docker: Replace git with curl

--- docker/build-daemon.sh
+++ docker/build-daemon.sh
@@ -1,5 +1,7 @@
-./autogen.sh
-./configure --disable-wallet --without-gui --with-incompatible-bdb
+#! /usr/bin/env bash
+
+# Causes the shell to exit if any subcommand or pipeline returns a non-zero status
+set -e
 
 DAEMON_TARGET=${1:-src/elementsd}
 NUM_JOBS=4
@@ -7,4 +9,6 @@ if [ -f /proc/cpuinfo ]; then
     NUM_JOBS=$(cat /proc/cpuinfo | grep ^processor | wc -l)
 fi
 
+./autogen.sh
+./configure --disable-wallet --without-gui --with-incompatible-bdb
 make $DAEMON_TARGET -j$NUM_JOBS
--- docker/daemon-deps.sh
+++ docker/daemon-deps.sh
@@ -6,7 +6,7 @@ set -e
 # sed -i 's/deb.debian.org/httpredir.debian.org/g' /etc/apt/sources.list
 apt-get update
 apt-get upgrade -yqq
-apt-get install -yqq git
+apt-get install -yqq curl
 apt-get install -yqq make build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev libzmq3-dev
 # apt-get install -yqq python curl build-essential libtool autotools-dev automake pkg-config bsdmainutils
 # rm -rf /var/lib/apt/lists/* /var/cache/* /tmp/* /usr/share/locale/* /usr/man /usr/share/doc /lib/xtables/libip6*
