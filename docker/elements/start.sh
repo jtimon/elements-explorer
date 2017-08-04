b468470f922cc4bc5d9c58beabdc5db1857acef0
Jorge Timón <jtimon@jtimon.cc>
2017-08-04 03:49:52 +0200

    Docker: Re-enable elementsregtest

---
+++ docker/elements/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+BRANCH_DIR=elements-fixtravis
+DATADIR=/root/.elements/
+PORT=7041
+
+# TODO Use Honcho or supervisord
+./$BRANCH_DIR/src/elementsd -regtest -datadir=$DATADIR -rpcbind=0.0.0.0:$PORT -printtoconsole -debug -logips -logtimestamps -server -txindex -rpcuser=user1 -rpcpassword=password1 -rpcallowip=10.5.0.5 -listen -discover -validatepegin=0
