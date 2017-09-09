e4fe9185a1137a624fc20a51acd8ce10b9b2036b
Jorge Timón <jtimon@jtimon.cc>
2017-09-09 08:11:49 +0200

    GUI: INtroduce controller:chain

---
+++ app/scripts/controllers/chain.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('rpcExplorerApp')
+    .controller('ChainCtrl', function ($scope, $routeParams, SrvChain, SrvUtil, SrvBackend) {
+
+        $scope.selected_chain = SrvChain.get();
+        $scope.available_chains = [$scope.selected_chain];
+
+        function successAvailableChains(data) {
+            $scope.available_chains = data["data"]["available_chains"];
+        }
+        SrvBackend.GetAvailableChains()
+            .then(SrvUtil.safeCb(successAvailableChains))
+            .catch(SrvUtil.errorCallbackScoped($scope));
+
+        function initChainCallback(data) {
+            $scope.chaininfo = data["data"]["result"];
+        };
+        $scope.ChangeChain = function () {
+            if ($scope.selected_chain) {
+                SrvChain.set($scope.selected_chain);
+            }
+            SrvBackend.rpcCall("getblockchaininfo", {}, initChainCallback, SrvUtil.errorCallbackScoped($scope));
+        };
+
+        if ($routeParams.chain) {
+            if ($routeParams.chain) {
+                $scope.selected_chain = $routeParams.chain;
+            }
+        }
+        $scope.ChangeChain();
+    });
