0a706e61c513113062d5383e61b39f182f0290f2
Jorge Timón <jtimon@jtimon.cc>
2017-09-18 23:18:15 +0200

    GUI: Cache getblockchaininfo (SrvChain)

--- app/scripts/services/chain.js
+++ app/scripts/services/chain.js
@@ -2,11 +2,13 @@
 /*global $:false */
 
 angular.module('rpcExplorerApp')
-    .service('SrvChain', function SrvChain($http, $rootScope) {
+    .service('SrvChain', function SrvChain($q, $http, $rootScope, SrvUtil) {
 
         var BACKEND_URL = '/api/v0';
         var srv = {};
+
         var selected_chain = "bitcoin";
+        var cache = {};
 
         srv.set = function(_selected_chain) {
             selected_chain = _selected_chain;
@@ -25,10 +27,16 @@ angular.module('rpcExplorerApp')
         }
 
         srv.GetChainInfo = function() {
-            return $http.post(BACKEND_URL + '/chain/' + srv.get() + '/' + "getblockchaininfo", {})
-                .then(function (response) {
-                    return response["data"]["result"];
+            var resource = 'getblockchaininfo';
+            SrvUtil.PreCache(cache, selected_chain, resource);
+            if (cache[selected_chain][resource][selected_chain]) {
+                return $q(function(resolve) {
+                    resolve(cache[selected_chain][resource][selected_chain]);
                 });
+            } else {
+                return $http.post(BACKEND_URL + '/chain/' + selected_chain + '/' + resource, {})
+                    .then(SrvUtil.CacheElem(cache, selected_chain, resource, selected_chain));
+            }
         }
 
         return srv;
