194a6a33d66277997dd0e3171c30398d4953e7c4
Jorge Timón <jtimon@jtimon.cc>
2017-10-01 06:32:40 +0200

    Docker: Python: Use honcho for the elements daemon

--- docker/elements/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-BRANCH_DIR=elements-e14-rpc-plotter
-DATADIR=/root/.elements/
-PORT=7041
-
-# TODO Use Honcho or supervisord
-./$BRANCH_DIR/src/elementsd -regtest -datadir=$DATADIR -rpcbind=0.0.0.0:$PORT -printtoconsole -debug -logips -logtimestamps -server -txindex -rpcuser=user1 -rpcpassword=password1 -rpcallowip=10.5.0.5 -listen -discover -validatepegin=0 -persistmempool
