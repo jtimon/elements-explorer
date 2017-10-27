9f0c7cfffdf375e85d6e86c7975e1b13907a00ca
Jorge Timón <jtimon@jtimon.cc>
2017-10-27 19:43:55 +0200

    Server: Daemons: Move rpc password to env variables

--- py/settings.py
+++ py/settings.py
@@ -37,7 +37,7 @@ RESOURCES_FOR_GET_BY_ID = [
 ]
 
 AVAILABLE_CHAINS = {
-    "bitcoin": RpcCaller("bitcoin:8532", 'user1', 'password1', RPC_ALLOWED_CALLS),
-    "testnet3": RpcCaller("bitcoin:18532", 'user1', 'password1', RPC_ALLOWED_CALLS),
-    "elementsregtest": RpcCaller("elements:7041", 'user1', 'password1', RPC_ALLOWED_CALLS),
+    "bitcoin": RpcCaller("bitcoin:8532", os.environ.get('BITCOIN_RPCUSER'), os.environ.get('BITCOIN_RPCPASSWORD'), RPC_ALLOWED_CALLS),
+    "testnet3": RpcCaller("bitcoin:18532", os.environ.get('TESTNET3_RPCUSER'), os.environ.get('TESTNET3_RPCPASSWORD'), RPC_ALLOWED_CALLS),
+    "elementsregtest": RpcCaller("elements:7041", os.environ.get('ELEMENTS_RPCUSER'), os.environ.get('ELEMENTS_RPCPASSWORD'), RPC_ALLOWED_CALLS),
 }
