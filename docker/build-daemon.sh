eb257b1f756cecdef0b472dc85a72e42caeb3bf4
Jorge Timón <jtimon@jtimon.cc>
2017-10-28 02:25:22 +0200

    Daemons: Move more from dockerfile to script

--- docker/build-daemon.sh
+++ docker/build-daemon.sh
@@ -3,12 +3,19 @@
 # Causes the shell to exit if any subcommand or pipeline returns a non-zero status
 set -e
 
-DAEMON_TARGET=${1:-src/elementsd}
+REPO_HOST=${1}
+REPO_NAME=${2}
+BRANCH_COMMIT=${3}
+MAKE_TARGET=${4}
+BRANCH_DIR=$REPO_NAME-$BRANCH_COMMIT
+BRANCH_URL=$REPO_HOST/$REPO_NAME/archive/$BRANCH_COMMIT.tar.gz
 NUM_JOBS=4
 if [ -f /proc/cpuinfo ]; then
     NUM_JOBS=$(cat /proc/cpuinfo | grep ^processor | wc -l)
 fi
 
+curl -L $BRANCH_URL | tar xz
+cd $BRANCH_DIR
 ./autogen.sh
 ./configure --disable-wallet --without-gui --with-incompatible-bdb
-make $DAEMON_TARGET -j$NUM_JOBS
+make $MAKE_TARGET -j$NUM_JOBS
