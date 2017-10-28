16e43699825246fedfede7fc5bc205557ea72c22
Jorge Timón <jtimon@jtimon.cc>
2017-10-29 01:47:04 +0200

    Server: Introduce daemon_subscriber.py

---
+++ lib/explorer/daemon_subscriber.py
@@ -0,0 +1,64 @@
+
+import binascii
+
+from lib import zmqmin
+from lib import minql
+
+from lib.explorer.explorer_server import GetById
+
+class DaemonSubscriber(zmqmin.Subscriber, zmqmin.Process):
+
+    def __init__(self, address,
+                 db_type,
+                 db_adr,
+                 db_name,
+                 db_user,
+                 db_pass,
+                 chain,
+                 rpccaller,
+                 worker_id='DaemonSubscriber',
+                 *args, **kwargs):
+
+        self.db_type = db_type
+        self.db_adr = db_adr
+        self.db_name = db_name
+        self.db_user = db_user
+        self.db_pass = db_pass
+        self.chain = chain
+        self.rpccaller = rpccaller
+        self.resource = 'chaininfo'
+
+        super(DaemonSubscriber, self).__init__(
+            address=address,
+            context=None, single=False,
+            worker_id=worker_id,
+            json=False,
+            topic='hashblock',
+            multipart=True,
+            *args, **kwargs)
+
+    def _init_process(self):
+        super(DaemonSubscriber, self)._init_process()
+        self.db_client = minql.MinqlClientFactory(self.db_type)(self.db_adr, self.db_name, self.db_user, self.db_pass)
+
+    def _loop(self):
+        while True:
+            msg_parts = self.receive_message()
+            block_hash = binascii.hexlify(msg_parts[1])
+
+            json_result = GetById(self.db_client, self.rpccaller, self.chain, 'block', block_hash)
+
+            entry = {}
+            entry['id'] = self.chain
+            entry['bestblockhash'] = block_hash
+            entry['blocks'] = json_result['height']
+            entry['mediantime'] = json_result['mediantime']
+            print('put entry', self.chain, self.resource, entry)
+            try:
+                db_result = self.db_client.put(self.resource, entry)
+            except:
+                print('FAILED ENTRY', self.chain, self.resource, entry)
+                continue
+            if not db_result:
+                print('FAILED ENTRY: No db result', self.chain, self.resource, entry)
+                continue
---
+++ py/bin/daemon_subscriber.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+if __name__ != '__main__':
+    raise ImportError(u"%s may only be run as a script" % __file__)
+
+import os
+
+from lib.explorer.daemon_subscriber import DaemonSubscriber
+from lib.explorer.rpcdaemon import RpcCaller
+
+# TODO unify with settings
+RPC_ALLOWED_CALLS = [
+    "getblockchaininfo",
+    "getblock",
+    "getblockhash",
+    "getrawtransaction",
+    "getblockstats",
+    "getmempoolinfo",
+    "getrawmempool",
+    "getmempoolentry",
+]
+
+AVAILABLE_CHAINS = {
+    "bitcoin": RpcCaller(os.environ.get('BITCOIN_ADR'),
+                         os.environ.get('BITCOIN_RPCUSER'),
+                         os.environ.get('BITCOIN_RPCPASSWORD'),
+                         RPC_ALLOWED_CALLS),
+    "testnet3": RpcCaller(os.environ.get('TESTNET3_ADR'),
+                          os.environ.get('TESTNET3_RPCUSER'),
+                          os.environ.get('TESTNET3_RPCPASSWORD'),
+                          RPC_ALLOWED_CALLS),
+    "elementsregtest": RpcCaller(os.environ.get('ELEMENTSREGTEST_ADR'),
+                                 os.environ.get('ELEMENTS_RPCUSER'),
+                                 os.environ.get('ELEMENTS_RPCPASSWORD'),
+                                 RPC_ALLOWED_CALLS),
+}
+
+daemon_subscriber = DaemonSubscriber(os.environ.get('BITCOIN_ZMQ'), os.environ.get('DB_TYPE'), os.environ.get('DB_ADR'), os.environ.get('DB_NAME'), os.environ.get('DB_USER'), os.environ.get('DB_PASS'), 'bitcoin', AVAILABLE_CHAINS['bitcoin'])
+daemon_subscriber.start()
+daemon_subscriber2 = DaemonSubscriber(os.environ.get('TESTNET3_ZMQ'), os.environ.get('DB_TYPE'), os.environ.get('DB_ADR'), os.environ.get('DB_NAME'), os.environ.get('DB_USER'), os.environ.get('DB_PASS'), 'testnet3', AVAILABLE_CHAINS['testnet3'])
+daemon_subscriber2.start()
+daemon_subscriber3 = DaemonSubscriber(os.environ.get('ELEMENTSREGTEST_ZMQ'), os.environ.get('DB_TYPE'), os.environ.get('DB_ADR'), os.environ.get('DB_NAME'), os.environ.get('DB_USER'), os.environ.get('DB_PASS'), 'elementsregtest', AVAILABLE_CHAINS['elementsregtest'])
+daemon_subscriber3.start()
