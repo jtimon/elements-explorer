2e9cb63085500262231eaa6058deef9d5aa02335
Jorge Timón <jtimon@jtimon.cc>
2017-10-31 19:57:37 +0100

    Server: MOVEONLY: Server: Move Decouple GetById to lib/explorer

---
+++ lib/explorer/explorer_server.py
@@ -0,0 +1,31 @@
+
+import json
+
+def RpcFromId(rpccaller, resource, req_id):
+    if resource == 'blockstats':
+        rpc_result = rpccaller.RpcCall('getblockstats', {'height': req_id})
+    elif resource == 'block':
+        rpc_result = rpccaller.RpcCall('getblock', {'blockhash': req_id})
+    elif resource == 'tx':
+        rpc_result = rpccaller.RpcCall('getrawtransaction', {'txid': req_id, 'verbose': 1})
+
+    return rpc_result
+
+def GetById(db_client, rpccaller, chain, resource, req_id):
+    try:
+        db_result = db_client.get(chain + "_" + resource, req_id)
+        if not db_result:
+            return {'error': {'message': 'No result db for %s.' % resource}}
+        if not 'blob' in db_result:
+            return {'error': {'message': 'No blob result db for %s.' % resource}}
+        json_result = json.loads(db_result['blob'])
+    except:
+        json_result = RpcFromId(rpccaller, resource, req_id)
+        if not json_result:
+            return {'error': {'message': 'No rpc result for %s.' % resource}}
+        db_cache = {}
+        db_cache['id'] = req_id
+        db_cache['blob'] = json.dumps(json_result)
+        db_client.put(chain + "_" + resource, db_cache)
+
+    return json_result
--- py/server.py
+++ py/server.py
@@ -2,6 +2,8 @@
 from flask import Blueprint, request, jsonify
 import json
 
+from lib.explorer.explorer_server import GetById
+
 import crossdomain
 from settings import *
 
@@ -12,35 +14,6 @@ API_URL = '/api/v0'
 @crossdomain.crossdomain(origin='*')
 def available_chains():
     return jsonify( {'available_chains': AVAILABLE_CHAINS.keys()} ), 200
-
-def RpcFromId(rpccaller, resource, req_id):
-    if resource == 'blockstats':
-        rpc_result = rpccaller.RpcCall('getblockstats', {'height': req_id})
-    elif resource == 'block':
-        rpc_result = rpccaller.RpcCall('getblock', {'blockhash': req_id})
-    elif resource == 'tx':
-        rpc_result = rpccaller.RpcCall('getrawtransaction', {'txid': req_id, 'verbose': 1})
-
-    return rpc_result
-
-def GetById(db_client, rpccaller, chain, resource, req_id):
-    try:
-        db_result = db_client.get(chain + "_" + resource, req_id)
-        if not db_result:
-            return {'error': {'message': 'No result db for %s.' % resource}}
-        if not 'blob' in db_result:
-            return {'error': {'message': 'No blob result db for %s.' % resource}}
-        json_result = json.loads(db_result['blob'])
-    except:
-        json_result = RpcFromId(rpccaller, resource, req_id)
-        if not json_result:
-            return {'error': {'message': 'No rpc result for %s.' % resource}}
-        db_cache = {}
-        db_cache['id'] = req_id
-        db_cache['blob'] = json.dumps(json_result)
-        db_client.put(chain + "_" + resource, db_cache)
-
-    return json_result
 
 @api_blueprint.route(API_URL + '/chain/<string:chain>/<string:resource>', methods = ['POST'])
 @crossdomain.crossdomain(origin='*')
