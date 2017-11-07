976abc739b9c76420b63612186feabb230e40190
Jorge Timón <jtimon@jtimon.cc>
2017-11-07 10:36:52 +0100

    Server: Have an independent subscriber per chain in Procfile

--- py/bin/daemon_subscriber.py
+++ py/bin/daemon_subscriber.py
@@ -2,12 +2,23 @@
 if __name__ != '__main__':
     raise ImportError(u"%s may only be run as a script" % __file__)
 
+import gflags
+
+gflags.DEFINE_string('chain', u"bitcoin",
+                     u"Chain to subscribe to")
+
+try:
+    import sys
+    argv = gflags.FLAGS(sys.argv)
+except gflags.FlagsError, e:
+    print('%s\n\nUsage %s ARGS \n%s' % (e, sys.argv[0], gflags.FLAGS))
+    sys.exit(0)
+FLAGS = gflags.FLAGS
+
+# ===----------------------------------------------------------------------===
+
 from lib.explorer.daemon_subscriber import DaemonSubscriber
 from lib.explorer.env_config import SUBSCRIBER_PARAMS
 
-daemon_subscriber = DaemonSubscriber(*SUBSCRIBER_PARAMS['bitcoin'])
+daemon_subscriber = DaemonSubscriber(*SUBSCRIBER_PARAMS[FLAGS.chain])
 daemon_subscriber.start()
-daemon_subscriber2 = DaemonSubscriber(*SUBSCRIBER_PARAMS['testnet3'])
-daemon_subscriber2.start()
-daemon_subscriber3 = DaemonSubscriber(*SUBSCRIBER_PARAMS['elementsregtest'])
-daemon_subscriber3.start()
