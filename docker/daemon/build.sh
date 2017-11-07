53f1f9a949c587235954389879195a34feb21193
Jorge Timón <jtimon@jtimon.cc>
2017-11-07 07:41:36 +0100

    Daemon: build.sh only takes env variables

--- docker/daemon/build.sh
+++ docker/daemon/build.sh
@@ -3,10 +3,8 @@
 # Causes the shell to exit if any subcommand or pipeline returns a non-zero status
 set -e
 
-REPO_HOST=${1}
-REPO_NAME=${2}
-BRANCH_DIR=$REPO_NAME-$BRANCH_COMMIT
-BRANCH_URL=$REPO_HOST/$REPO_NAME/archive/$BRANCH_COMMIT.tar.gz
+BRANCH_DIR=$DAEMON_NAME-$BRANCH_COMMIT
+BRANCH_URL=$REPO_HOST/$DAEMON_NAME/archive/$BRANCH_COMMIT.tar.gz
 NUM_JOBS=4
 if [ -f /proc/cpuinfo ]; then
     NUM_JOBS=$(cat /proc/cpuinfo | grep ^processor | wc -l)
@@ -16,4 +14,4 @@ curl -L $BRANCH_URL | tar xz
 cd $BRANCH_DIR
 ./autogen.sh
 ./configure --disable-wallet --without-gui --with-incompatible-bdb
-make "src/"$REPO_NAME"d" -j$NUM_JOBS
+make "src/"$DAEMON_NAME"d" -j$NUM_JOBS
