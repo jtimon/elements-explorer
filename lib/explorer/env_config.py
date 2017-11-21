eb19834e893c1c087e55c2ef74cc8a0ebb44dc0b
Jorge Timón <jtimon@jtimon.cc>
2017-11-21 10:56:53 +0100

    Server: Refactor: Put zmq param in AVAILABLE_CHAINS

--- lib/explorer/env_config.py
+++ lib/explorer/env_config.py
@@ -22,23 +22,26 @@ AVAILABLE_CHAINS = {
                          os.environ.get('BITCOIN_RPCUSER'),
                          os.environ.get('BITCOIN_RPCPASSWORD')
         ),
+        'zmq': os.environ.get('BITCOIN_ZMQ'),
     },
     "testnet3": {
         'rpc': RpcCaller(os.environ.get('TESTNET3_ADR'),
                          os.environ.get('TESTNET3_RPCUSER'),
                          os.environ.get('TESTNET3_RPCPASSWORD')
         ),
+        'zmq': os.environ.get('TESTNET3_ZMQ'),
     },
     "elementsregtest": {
         'rpc': RpcCaller(os.environ.get('ELEMENTSREGTEST_ADR'),
                          os.environ.get('ELEMENTS_RPCUSER'),
                          os.environ.get('ELEMENTS_RPCPASSWORD')
         ),
+        'zmq': os.environ.get('ELEMENTSREGTEST_ZMQ'),
     },
 }
 
 SUBSCRIBER_PARAMS = {
-    'bitcoin': [os.environ.get('BITCOIN_ZMQ'), 'bitcoin', AVAILABLE_CHAINS['bitcoin']['rpc'], DB_FACTORY],
-    'testnet3': [os.environ.get('TESTNET3_ZMQ'), 'testnet3', AVAILABLE_CHAINS['testnet3']['rpc'], DB_FACTORY],
-    'elementsregtest': [os.environ.get('ELEMENTSREGTEST_ZMQ'), 'elementsregtest', AVAILABLE_CHAINS['elementsregtest']['rpc'], DB_FACTORY],
+    'bitcoin': [AVAILABLE_CHAINS['bitcoin']['zmq'], 'bitcoin', AVAILABLE_CHAINS['bitcoin']['rpc'], DB_FACTORY],
+    'testnet3': [AVAILABLE_CHAINS['testnet3']['zmq'], 'testnet3', AVAILABLE_CHAINS['testnet3']['rpc'], DB_FACTORY],
+    'elementsregtest': [AVAILABLE_CHAINS['elementsregtest']['zmq'], 'elementsregtest', AVAILABLE_CHAINS['elementsregtest']['rpc'], DB_FACTORY],
 }
