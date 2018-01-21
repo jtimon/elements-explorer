acfa9962b6660246d2b880f16e22ee3387b9d545
Jorge Timón <jtimon@jtimon.cc>
2018-01-21 07:16:50 +0100

    Server: Config: Separate processes' arguments by chain

--- lib/explorer/env_config.py
+++ lib/explorer/env_config.py
@@ -55,18 +55,51 @@ AVAILABLE_CHAINS = {
     },
 }
 
+REORG_CRON_PARAMS = {
+    "bitcoin": [60, 300], # every 1 min after 5 min
+    "testnet3": [60, 60 * 60], # every 1 min after 1 hour
+    "elementsregtest": [60, 60 * 5], # every 1 min after 5 min
+}
+
+MEMPOOL_CACHER_PARAMS = {
+    "bitcoin": [60, 60], # every 1 min after 1 min
+    "testnet3": [60, 60], # every 1 min after 1 min
+    "elementsregtest": [60 * 20, 60], # every 20 min after 1 min
+}
+
+# 2 hours before starting, change after https://github.com/bitcoin/bitcoin/issues/12142 is fixed
+SECONDS_FIX_12142 = 60 * 60 * 2 # 2 hours
+MEMPOOL_SAVER_PARAMS = {
+    "bitcoin": [60, SECONDS_FIX_12142], # every 1 min after SECONDS_FIX_12142 seconds
+    "testnet3": [60, SECONDS_FIX_12142], # every 1 min after SECONDS_FIX_12142 seconds
+    "elementsregtest": [60 * 60 * 24 * 7, SECONDS_FIX_12142], # every 1 week after SECONDS_FIX_12142 seconds
+}
+
+GREEDY_CACHER_PARAMS = {
+    "bitcoin": [60 * 60, 60 * 60], # every 1 hour after 1 hour
+    "testnet3": [60 * 60, 60 * 60 * 24], # every 1 hour after 1 day
+    "elementsregtest": [60 * 60, 60 * 60], # every 1 hour after 1 hour
+}
+
 def subscriber_params(chain):
     return [AVAILABLE_CHAINS[chain]['zmq'], chain, AVAILABLE_CHAINS[chain]['rpc'], AVAILABLE_CHAINS[chain]['db']]
 
 def reorg_cron_params(chain):
-    return [chain, AVAILABLE_CHAINS[chain]['rpc'], AVAILABLE_CHAINS[chain]['db'].create(), 60, 300] # 5 min
+    to_return = [chain, AVAILABLE_CHAINS[chain]['rpc'], AVAILABLE_CHAINS[chain]['db'].create()]
+    to_return.extend(REORG_CRON_PARAMS[chain])
+    return to_return
 
 def mempool_cacher_params(chain):
-    return [chain, AVAILABLE_CHAINS[chain]['rpc'], AVAILABLE_CHAINS[chain]['db'].create(), 60, 60] # 1 min
+    to_return = [chain, AVAILABLE_CHAINS[chain]['rpc'], AVAILABLE_CHAINS[chain]['db'].create()]
+    to_return.extend(MEMPOOL_CACHER_PARAMS[chain])
+    return to_return
 
 def mempool_saver_params(chain):
-    # 2 hours before starting, change after https://github.com/bitcoin/bitcoin/issues/12142 is fixed
-    return [chain, AVAILABLE_CHAINS[chain]['rpc'], 60, 7200] # 2 hours
+    to_return = [chain, AVAILABLE_CHAINS[chain]['rpc']]
+    to_return.extend(MEMPOOL_SAVER_PARAMS[chain])
+    return to_return
 
 def greedy_cacher_params(chain):
-    return [chain, AVAILABLE_CHAINS[chain]['rpc'], AVAILABLE_CHAINS[chain]['db'].create(), 60, 3600] # 1 hour
+    to_return = [chain, AVAILABLE_CHAINS[chain]['rpc'], AVAILABLE_CHAINS[chain]['db'].create()]
+    to_return.extend(GREEDY_CACHER_PARAMS[chain])
+    return to_return
