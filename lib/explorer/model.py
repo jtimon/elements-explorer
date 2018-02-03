89222ecdfc3b03ca9550135d9728fc275b7646ad
Jorge Timón <jtimon@jtimon.cc>
2018-02-03 01:45:12 +0100

    DB: Introduce Ormin model of the database

---
+++ lib/explorer/model.py
@@ -0,0 +1,31 @@
+
+from mintools import ormin
+
+class Chaininfo(ormin.Model):
+    bestblockhash = ormin.StringField()
+    blocks = ormin.IntField()
+    mediantime = ormin.IntField()
+
+class Block(ormin.Model):
+    height = ormin.IntField(index=True)
+    blob = ormin.TextField()
+
+class Tx(ormin.Model):
+    blockhash = ormin.StringField(index=True)
+    blob = ormin.TextField()
+
+class Blockstats(ormin.Model):
+    height = ormin.IntField(index=True)
+    blob = ormin.TextField()
+
+class Mempoolstats(ormin.Model):
+    time = ormin.IntField(index=True)
+    blob = ormin.TextField()
+
+ORMIN_DOMAIN = ormin.Domain([
+    Chaininfo,
+    Block,
+    Tx,
+    Blockstats,
+    Mempoolstats,
+])
