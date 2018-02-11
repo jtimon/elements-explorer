f87bad70e0566d56f386543170d3d3026ef7b748
Jorge Timón <jtimon@jtimon.cc>
2018-02-11 06:44:42 +0100

    Deployment: Change greedy cacher params

--- explorer/env_config.py
+++ explorer/env_config.py
@@ -75,8 +75,8 @@ MEMPOOL_SAVER_PARAMS = {
 }
 
 GREEDY_CACHER_PARAMS = {
-    "bitcoin": [60 * 60, 60 * 60, True], # every 1 hour after 1 hour
-    "testnet3": [60 * 60, 60 * 60 * 24 * 2, True], # every 1 hour after 2 days
+    "bitcoin": [60, 60 * 60 * 24, True], # every 1 min after 1 day, cache txs
+    "testnet3": [60, 60 * 60, True], # every 1 min after 1 hour, cache txs
     "elementsregtest": [60 * 60, 60 * 60, True], # every 1 hour after 1 hour, cache txs
 }
 
