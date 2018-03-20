b8e110137c74adcb636150079b13ab766777ad58
Jorge Timón <jtimon@jtimon.cc>
2018-03-20 03:41:19 +0100

    Config: reorg_cron after 5 min for testnet3 too

--- explorer/env_config.py
+++ explorer/env_config.py
@@ -58,8 +58,8 @@ AVAILABLE_CHAINS = {
 }
 
 REORG_CRON_PARAMS = {
-    "bitcoin": [60, 300], # every 1 min after 5 min
-    "testnet3": [60, 60 * 60], # every 1 min after 1 hour
+    "bitcoin": [60, 60 * 5], # every 1 min after 5 min
+    "testnet3": [60, 60 * 5], # every 1 min after 5 min
     "elementsregtest": [60, 60 * 5], # every 1 min after 5 min
 }
 
