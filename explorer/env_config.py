a0d925b3c3c716a9ea0d7d6f141c2de2f788ff46
Jorge Timón <jtimon@jtimon.cc>
2018-03-20 05:06:02 +0100

    Config: testnet3: Greedy cache after 1 hour instead of 5 min

--- explorer/env_config.py
+++ explorer/env_config.py
@@ -79,7 +79,7 @@ MEMPOOL_SAVER_PARAMS = {
 
 GREEDY_CACHER_PARAMS = {
     "bitcoin": [60, 60 * 60 * 24, True], # every 1 min after 1 day, cache txs
-    "testnet3": [60, 60 * 5, True], # every 1 min after 5 min, cache txs
+    "testnet3": [60, 60 * 60, True], # every 1 min after 1 hour, cache txs
     "elementsregtest": [60 * 60, 60 * 5, True], # every 1 hour after 5 min, cache txs
 }
 
