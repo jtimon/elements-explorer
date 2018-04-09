ed80323b1453aa7597b2e3bdcd547ea752542ad1
Jorge Timón <jtimon@jtimon.cc>
2018-04-09 20:00:50 +0200

    GUI: Simplify chain controller

--- gui/scripts/controllers/chain.js
+++ gui/scripts/controllers/chain.js
@@ -3,33 +3,21 @@
 angular.module('rpcExplorerApp')
     .controller('ChainCtrl', function ($scope, $rootScope, $location, SrvChain, SrvUtil) {
 
-        function successAvailableChains(data) {
-            $scope.available_chains = data;
-        }
         SrvChain.GetAvailableChains()
-            .then(successAvailableChains)
+            .then(function (available_chains) {
+                $scope.available_chains = available_chains;
+            })
             .catch(SrvUtil.errorCallbackScoped($scope));
 
-        function initChainCallback(data) {
-            $rootScope.chaininfo = data;
-        };
-
         $scope.ChangeChain = function () {
-            if ($scope.selected_chain) {
-                SrvChain.set($scope.selected_chain, $scope);
-                $location.path($location.path().replace(/chain\/(.+?)\/(.*)/g,"chain/" + $scope.selected_chain + "/$2"));
-            }
-        };
-
-        $scope.ChangeChainRoute = function () {
-            if ($scope.selected_chain) {
-                SrvChain.GetChainInfo()
-                    .then(initChainCallback)
-                    .catch(SrvUtil.errorCallbackScoped($scope));
-            }
+            $location.path($location.path().replace(/chain\/(.+?)\/(.*)/g,"chain/" + $scope.selected_chain + "/$2"));
         };
 
         $scope.$on('$routeChangeSuccess', function() {
-            $scope.ChangeChainRoute();
+            SrvChain.GetChainInfo()
+                .then(function (chaininfo) {
+                    $rootScope.chaininfo = chaininfo;
+                })
+                .catch(SrvUtil.errorCallbackScoped($scope));
         });
     });
