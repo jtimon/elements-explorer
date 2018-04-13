89fd1defc3cfc7d746df0ceb50296cb5d6ae321e
Michael Goldstein <michael@bitstein.org>
2018-04-13 12:00:57 -0500

    GUI2: jumbotron_recent_blocks.jsx: Remove nonfunctional dropdowns

--- gui2/src/components/jumbotron_recent_blocks.jsx
+++ gui2/src/components/jumbotron_recent_blocks.jsx
@@ -4,30 +4,6 @@ function RecentBlocksJumbotron() {
   return (
     <div className="container">
       <h1>Recent Blocks</h1>
-      <div className="block-dropdowns">
-        <div className="date-dropdown">
-          <div>
-            <img alt="" src="/gui2/static/img/icons/calendar.svg" />
-          </div>
-          <div>
-            <span>Feb 12, 2018</span>
-          </div>
-          <div>
-            <img alt="" src="/gui2/static/img/icons/Arrow-down.svg" />
-          </div>
-        </div>
-        <div className="time-dropdown">
-          <div>
-            <img alt="" src="/gui2/static/img/icons/time.svg" />
-          </div>
-          <div>
-            <span>12:00 - 16:00</span>
-          </div>
-          <div>
-            <img alt="" src="/gui2/static/img/icons/Arrow-down.svg" />
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
