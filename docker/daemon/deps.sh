c19d219b53b5acf8eb8643e91201bdafc0d89001
Jorge Timón <jtimon@jtimon.cc>
2018-04-17 03:13:13 +0200

    Docker: Install tor and Separate daemon base dependencies by line

--- docker/daemon/deps.sh
+++ docker/daemon/deps.sh
@@ -4,8 +4,25 @@
 set -e
 
 # sed -i 's/deb.debian.org/httpredir.debian.org/g' /etc/apt/sources.list
-apt-get update
-apt-get install -yqq curl
-apt-get install -yqq make build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev libzmq3-dev
-# apt-get install -yqq python curl build-essential libtool autotools-dev automake pkg-config bsdmainutils
+apt-get update && apt-get install -yqq \
+        curl \
+        make \
+        build-essential \
+        libtool \
+        autotools-dev \
+        automake \
+        pkg-config \
+        libssl-dev \
+        libevent-dev \
+        bsdmainutils \
+        libboost-system-dev \
+        libboost-filesystem-dev \
+        libboost-chrono-dev \
+        libboost-program-options-dev \
+        libboost-test-dev \
+        libboost-thread-dev \
+        libminiupnpc-dev \
+        libzmq3-dev \
+        tor \
+
 # rm -rf /var/lib/apt/lists/* /var/cache/* /tmp/* /usr/share/locale/* /usr/man /usr/share/doc /lib/xtables/libip6*
