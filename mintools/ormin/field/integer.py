d304e4f2c1567721c10bf58c78ce32ba68921e24
Jorge Timón <jtimon@jtimon.cc>
2018-05-28 16:19:47 +0200

    Mintools: Ormin: Validate that a an int is not out of range...
    before trying to insert it

--- mintools/ormin/field/integer.py
+++ mintools/ormin/field/integer.py
@@ -1,9 +1,13 @@
 
 from .number import NumericField
 
+MAX_INT = 2147483647
+
 class IntField(NumericField):
 
     def __init__(self, **kwargs):
+        if not 'lt' in kwargs or kwargs['lt'] == None or kwargs['lt'] > MAX_INT:
+            kwargs['lt'] = MAX_INT
         super(IntField, self).__init__(field_type='int', **kwargs)
 
     def _clean_value(self, value):
