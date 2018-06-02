062eaa93a9fafcc50206ecb24cc988d28239f735
Jorge Timón <jtimon@jtimon.cc>
2018-06-02 18:35:26 +0200

    f'Server: Models: MOVEONLY: s/model.Tx/models.transaction.Tx/'

--- explorer/domain.py
+++ explorer/domain.py
@@ -1,13 +1,13 @@
 
 from mintools import ormin
 
-from explorer.model import *
+from explorer.model import Chaininfo, Block
 from explorer import models
 
 ORMIN_DOMAIN = ormin.Domain([
     Chaininfo,
     Block,
-    Tx,
+    models.transaction.Tx,
     models.stats.Blockstats,
     models.stats.Mempoolstats,
 ])
