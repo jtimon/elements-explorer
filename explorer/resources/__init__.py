d749741e2f1a0d11e96aa1a3db84dd83565c2cd6
Jorge Timón <jtimon@jtimon.cc>
2018-06-07 08:24:25 +0200

    MOVEONLY: Server: Move AddressResource inside resources

--- explorer/resources/__init__.py
+++ explorer/resources/__init__.py
@@ -1,6 +1,7 @@
 
 from .chain import UnknownChainError, ChainResource
 
+import address
 import blockheight
 import mempoolstats
 import rpccaller
---
+++ explorer/resources/address.py
@@ -0,0 +1,77 @@
+
+import json
+
+from explorer import models, services
+
+from .chain import UnknownChainError, ChainResource
+
+class AddressResource(ChainResource):
+
+    def search_by_address(self, height, addresses):
+
+        block = services.GetBlockByHeight(self.rpccaller, height)
+        if isinstance(block, dict) and 'error' in block:
+            return block
+
+        receipts = []
+        expenditures = []
+        tx_ids = json.loads(block.tx)
+        for txid in tx_ids:
+            orm_tx = models.transaction.Tx.get(txid)
+            if isinstance(orm_tx, dict) and 'error' in orm_tx:
+                return orm_tx
+            elif not isinstance(orm_tx, models.transaction.Tx):
+                print('Error in AddressResource.search_by_address: wrong type for tx', txid, orm_tx)
+                return {'error': {'message': 'Error getting tx %s (address)' % txid}}
+            tx = json.loads(orm_tx.blob)
+
+            for output in tx['vout']:
+                for address in addresses:
+                    if 'addresses' in output['scriptPubKey'] and address in output['scriptPubKey']['addresses']:
+                        receipts.append({'output': output, 'txid': txid, 'height': block.height})
+
+            for tx_input in tx['vin']:
+                if 'txid' in tx_input and 'vout' in tx_input:
+                    orm_tx_in = models.transaction.Tx.get(tx_input['txid'])
+                    if isinstance(orm_tx_in, dict) and 'error' in orm_tx_in:
+                        return orm_tx_in
+                    elif not isinstance(orm_tx_in, models.transaction.Tx):
+                        print('Error in AddressResource.search_by_address_ascendant: wrong type for tx', tx_input['txid'], orm_tx_in)
+                        return {'error': {'message': 'Error getting tx %s (address)' % tx_input['txid']}}
+                    tx_in = json.loads(orm_tx_in.blob)
+                    print('tx_in', tx_in, "tx_input['vout']", tx_input['vout'])
+                    output = tx_in['vout'][ tx_input['vout'] ]
+                    for address in addresses:
+                        if 'addresses' in output['scriptPubKey'] and address in output['scriptPubKey']['addresses']:
+                            expenditures.append({'input': tx_input, 'prev_output': output, 'txid': tx['txid'], 'height': block.height})
+
+        return {'expenditures': expenditures, 'receipts': receipts}
+
+    def resolve_request(self, req):
+        try:
+            req['json'] = self.update_chain(req['json'])
+        except UnknownChainError:
+            return {'error': {'message': 'Chain "%s" not supported.' % self.chain}}, 400
+
+        request = req['json']
+        if not 'addresses' in request:
+            return {'error': {'message': 'No addresses specified for %s with request %s' % ('address', request)}}, 400
+        if not 'start_height' in request:
+            return {'error': {'message': 'No start_height specified for %s with request %s' % ('address', request)}}, 400
+        if not 'end_height' in request:
+            return {'error': {'message': 'No end_height specified for %s with request %s' % ('address', request)}}, 400
+
+        if request['start_height'] < 1 or request['end_height'] < 1:
+            return {'error': {'message': 'Minimum height is 1 for %s' % ('address')}}, 400
+
+        json_result = {'expenditures': [], 'receipts': []}
+        for height in xrange(request['start_height'], request['end_height'] + 1):
+            address_block_result = self.search_by_address(height, request['addresses'])
+            if 'error' in address_block_result:
+                return {'error': address_block_result['error']}, 400
+            if 'expenditures' in address_block_result:
+                json_result['expenditures'].extend(address_block_result['expenditures'])
+            if 'receipts' in address_block_result:
+                json_result['receipts'].extend(address_block_result['receipts'])
+
+        return json_result, 200
