1f5d53d87bbfe349137c9281fe449310b552e01f
Jorge Timón <jtimon@jtimon.cc>
2018-06-15 03:07:01 +0200

    wip: Faucetinfo

--- explorer/models/faucet.py
+++ explorer/models/faucet.py
@@ -1,22 +1,32 @@
 
+import datetime
+
 from mintools import ormin
 from explorer.models.rpc_cached import RpcCachedModel
 
 class Faucetinfo(RpcCachedModel):
     time = ormin.IntField(index=True)
 
-    amount = ormin.IntField()
     balance = ormin.IntField()
+    donation_address = ormin.StringField()
+    giveaway_amount = ormin.IntField()
 
     @classmethod
     def truth_src_get(cls, req_id):
-        balance = super(Faucetinfo, cls)._rpccaller.RpcCall('getbalance', {'blockhash': req_id})
+
+        newaddress = super(Faucetinfo, cls)._rpccaller.RpcCall('getnewaddress')
+        if 'error' in newaddress:
+            return newaddress
+
+        balance = super(Faucetinfo, cls)._rpccaller.RpcCall('getbalance')
         if 'error' in balance:
             return balance
+        giveaway_amount = balance / 100
 
         faucetinfo = Faucetinfo(json_dict={
-            'time': time,
-            'amount': amount,
+            'time': datetime.datetime.now(),
+            'donation_address': donation_address,
+            'giveaway_amount': giveaway_amount,
             'balance': balance,
         })
         faucetinfo.id = req_id
