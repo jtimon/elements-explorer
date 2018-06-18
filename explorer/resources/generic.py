ad4dc80342c9f038266c485a62a7e0924024cc02
Jorge Timón <jtimon@jtimon.cc>
2018-06-18 19:32:19 +0200

    Server: Remove unused uses_blob option from GetByIdResource

--- explorer/resources/generic.py
+++ explorer/resources/generic.py
@@ -8,13 +8,12 @@ from .chain import UnknownChainError, ChainResource
 
 class GetByIdResource(ChainResource):
 
-    def __init__(self, resource, model, chain_required_properties=[], uses_blob=False, *args, **kwargs):
+    def __init__(self, resource, model, chain_required_properties=[], *args, **kwargs):
         super(GetByIdResource, self).__init__(*args, **kwargs)
 
         self.resource = resource
         self.model = model
         self.chain_required_properties = chain_required_properties
-        self.uses_blob = uses_blob
 
     def resolve_request(self, req):
         try:
@@ -38,34 +37,19 @@ class GetByIdResource(ChainResource):
 
         if not db_result:
             return {'error': {'message': 'No result db for %s %s.' % (self.resource, request['id'])}}, 400
-        elif isinstance(db_result, dict) and 'error' in db_result:
-            if isinstance(db_result['error'], dict) and 'message' in db_result['error']:
-                return {'error': {'message': db_result['error']['message']}}, 400
-            else:
-                return {'error': {'message': db_result['error']}}, 400
-
-        if not (isinstance(db_result, dict) or isinstance(db_result, ormin.Model)):
+        elif isinstance(db_result, dict):
+            response = db_result
+        elif isinstance(db_result, ormin.Model):
+            response = db_result.json()
+        else:
             print('ERROR: getting %s. db_result:' % self.resource, db_result)
             return {'error': {'message': 'Error getting %s.' % self.resource}}, 400
 
-        if self.uses_blob:
-            if isinstance(db_result, dict):
-                json_result = db_result
-            elif isinstance(db_result, ormin.Model):
-                json_result = db_result.json()
-
-            if not 'blob' in json_result:
-                print('ERROR: No blob result db for %s. db_result:' % self.resource, db_result)
-                return {'error': {'message': 'No blob result db for %s.' % self.resource}}, 400
-            response = json.loads(json_result['blob'])
-        else:
-            if isinstance(db_result, dict):
-                response = db_result
-            elif isinstance(db_result, ormin.Model):
-                response = db_result.json()
-
         if 'error' in response:
-            return {'error': response['error']}, 400
+            if isinstance(response['error'], dict) and 'message' in response['error']:
+                return {'error': {'message': response['error']['message']}}, 400
+            else:
+                return {'error': {'message': response['error']}}, 400
         if 'errors' in response:
-            return {'error': response['errors']}, 400
+            return {'error': {'message': response['errors']}}, 400
         return response, 200
