39eb59ead510e2ca2adbab1147a5b75468306e34
Jorge Timón <jtimon@jtimon.cc>
2018-08-17 02:25:44 +0200

    Docker: Daemons: Use regular args instead of env vars...
    for docker/daemon/build-with-wallet.sh and docker/daemon/Dockerfile

--- docker/daemon/build-with-wallet.sh
+++ docker/daemon/build-with-wallet.sh
@@ -3,8 +3,14 @@
 # Causes the shell to exit if any subcommand or pipeline returns a non-zero status
 set -e
 
-BRANCH_DIR=$REPO_NAME-$BRANCH_COMMIT
-BRANCH_URL=$REPO_HOST/$REPO_NAME/archive/$BRANCH_COMMIT.tar.gz
+# $1=BRANCH_COMMIT $2=REPO_NAME $3=REPO_HOST $4=DAEMON_NAME
+
+if [ "$4" = "" -o "$4" = "disabled_daemon" ]; then
+    exit 0
+fi
+
+BRANCH_DIR=$2-$1
+BRANCH_URL=$3/$2/archive/$1.tar.gz
 NUM_JOBS=4
 if [ -f /proc/cpuinfo ]; then
     NUM_JOBS=$(cat /proc/cpuinfo | grep ^processor | wc -l)
@@ -17,5 +23,5 @@ curl -L $BRANCH_URL | tar xz
 cd $BRANCH_DIR
 ./autogen.sh
 ./configure --without-gui --with-incompatible-bdb
-make "src/"$DAEMON_NAME"d" -j$NUM_JOBS
-make "src/"$DAEMON_NAME"-cli" -j$NUM_JOBS
+make "src/"$4"d" -j$NUM_JOBS
+make "src/"$4"-cli" -j$NUM_JOBS
