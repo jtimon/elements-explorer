acb093fccd816af69aa1772caf2a1196b67ea871
Jorge Timón <jtimon@jtimon.cc>
2018-10-04 06:38:51 +0200

    Faucet: Optionally add recaptcha on the backend side
    To add this functionality, create a file /explorer-data/keys/RECAPTCHA_SECRET.txt
    with your recaptcha backend key

--- explorer/resources/faucet.py
+++ explorer/resources/faucet.py
@@ -3,6 +3,8 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import datetime
+import json
+import requests
 
 import mintools
 
@@ -15,6 +17,17 @@ COIN = 100000000
 MIN_KEEP = 0.5
 MIN_SATOSHIS_SEND = 1000
 MAX_SATOSHIS_SEND = COIN
+
+# Load RECAPTCHA_SECRET or use not working default
+try:
+    with open('/build_docker/keys/RECAPTCHA_SECRET.txt', 'r') as f:
+        recaptcha_list = f.read().splitlines()
+        assert len(recaptcha_list) == 1
+        RECAPTCHA_SECRET = recaptcha_list[0]
+        print('BlockGenerator.import_keys_if_any: Rpc importprivkey result', result)
+except IOError:
+    print('ERROR: File /build_docker/keys/RECAPTCHA_SECRET.txt not found. Using public default...')
+    RECAPTCHA_SECRET = 'no_recaptcha'
 
 def get_balance(rpccaller, chain):
     balance_dict = rpccaller.RpcCall('getbalance', {})
@@ -62,15 +75,47 @@ class FaucetInfoResource(ChainResource):
 
 class FreeCoinsResource(ChainResource):
 
+    def check_recaptcha_response():
+        if not 'g-recaptcha-response' in request:
+            return {'error': {'message': 'No g-recaptcha-response specified to get %s in request %s' % ('freecoins', request)}}, 400
+
+        r = requests.post("https://www.google.com/recaptcha/api/siteverify",
+                          data={'secret': RECAPTCHA_SECRET,
+                                'response': request['g-recaptcha-response'],
+                                'remoteip': req['ip']
+                          })
+        print(r.status_code, r.reason)
+        if r.status_code != 200:
+            return {'error': {'message': '%s: Recaptcha failed %s' % ('freecoins', r.reason)}}, 400
+
+        print(r.text)
+        try:
+            recaptcha_response = json.loads(r.text)
+        except Exception as e:
+            print("Error in FreeCoinsResource:", type(e), e)
+            return {'error': {'message': '%s: Recaptcha failed %s' % ('freecoins', r.text)}}, 400
+
+        if not 'success' in recaptcha_response or not recaptcha_response['success']:
+            return {'error': {'message': '%s: Recaptcha failed %s' % ('freecoins', r.text)}}, 400
+        return {}
+
     def resolve_request(self, req):
         try:
             req['json'] = self.update_chain(req['json'])
         except UnknownChainError:
             return {'error': {'message': 'Chain "%s" not supported.' % self.chain}}, 400
 
+        if not 'ip' in req:
+            return {'error': {'message': 'No ip specified to get %s in request %s' % ('freecoins', req)}}, 400
+
         request = req['json']
         if not 'address' in request:
             return {'error': {'message': 'No address specified to get %s in request %s' % ('freecoins', request)}}, 400
+
+        if RECAPTCHA_SECRET != 'no_recaptcha':
+            response = self.check_recaptcha_response()
+            if 'error' in response:
+                return {'error': {'message': '%s: Recaptcha failed: %s' % ('freecoins', response['error'])}}, 400
 
         address = request['address']
         balance = get_balance(self.rpccaller, self.chain)
